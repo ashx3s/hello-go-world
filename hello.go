9580f64090f7eb0d16674931f0c52ff808c738ae
lilyx13 <ashlyn@lilyx.io>
2023-06-20 11:58:56 -0600

    hello world function code
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ hello.go
@@ -0,0 +1,9 @@
+// declare main package
+package main
+// import fmt library for formatting and printing text
+import "fmt"
+// implement the main function and print the text to console
+func main() {
+	fmt.Println("Hello, world!")
+}
+// run the app with `go run .`
